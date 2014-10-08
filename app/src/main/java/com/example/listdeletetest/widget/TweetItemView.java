0529267efd8afd23d5494124a68b418c22490eb1
Steffen Funke <info@steffen-funke.de>
2014-10-08 09:19:29 +0200

    custom list divider

--- app/src/main/java/com/example/listdeletetest/widget/TweetItemView.java
+++ app/src/main/java/com/example/listdeletetest/widget/TweetItemView.java
@@ -19,6 +19,7 @@ package com.example.listdeletetest.widget;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -33,56 +34,73 @@ import com.squareup.picasso.Picasso;
 
 
 public class TweetItemView extends RelativeLayout implements Checkable {
-    private final ImageView mProfileImage;
-    private final TextView mAuthorText;
-    private final TextView mMessageText;
-    private final ImageView mPostImage;
+	private final ImageView mProfileImage;
+	private final TextView mAuthorText;
+	private final TextView mMessageText;
+	private final ImageView mPostImage;
 	private boolean mChecked;
+	private Paint mDividerPaint;
+	private final Paint mSelectionPaint;
+//	private final Drawable mDividerDrawable;
 
 	public TweetItemView(Context context) {
-        super(context);
-	    LayoutInflater.from(context).inflate(R.layout.tweet_composite_view, this, true);
-	    int padding = getResources().getDimensionPixelSize(R.dimen.tweet_padding);
-	    setPadding(padding, padding, padding, padding);
-	    mProfileImage = (ImageView) findViewById(R.id.profile_image);
-	    mAuthorText = (TextView) findViewById(R.id.author_text);
-	    mMessageText = (TextView) findViewById(R.id.message_text);
-	    mPostImage = (ImageView) findViewById(R.id.post_image);
+		super(context);
+		LayoutInflater.from(context).inflate(R.layout.tweet_composite_view, this, true);
+		int padding = getResources().getDimensionPixelSize(R.dimen.tweet_padding);
+		setPadding(padding, padding, padding, padding);
+		mProfileImage = (ImageView) findViewById(R.id.profile_image);
+		mAuthorText = (TextView) findViewById(R.id.author_text);
+		mMessageText = (TextView) findViewById(R.id.message_text);
+		mPostImage = (ImageView) findViewById(R.id.post_image);
+
+		// custom divider, since we disabled list divider
+		mDividerPaint = new Paint();
+		mDividerPaint.setColor(0xffb7b7b7);
+
+		// cell selection
+		mSelectionPaint = new Paint();
+		mSelectionPaint.setColor(Color.argb(55, 255, 0, 0));
 	}
 
-    @Override
-    public boolean shouldDelayChildPressedState() {
-        return false;
-    }
+	@Override
+	public boolean shouldDelayChildPressedState() {
+		return false;
+	}
 
-    public void update(Tweet tweet) {
-        mAuthorText.setText(tweet.getAuthorName());
-        mMessageText.setText(tweet.getMessage());
+	public void update(Tweet tweet) {
+		mAuthorText.setText(tweet.getAuthorName());
+		mMessageText.setText(tweet.getMessage());
 
-        final Context context = getContext();
-	    Picasso.with(context)
-			    .load(tweet.getProfileImageUrl())
-			    .placeholder(R.drawable.tweet_placeholder_image)
-			    .error(R.drawable.tweet_placeholder_image)
-			    .into(mProfileImage);
+		final Context context = getContext();
+		Picasso.with(context)
+				.load(tweet.getProfileImageUrl())
+				.placeholder(R.drawable.tweet_placeholder_image)
+				.error(R.drawable.tweet_placeholder_image)
+				.into(mProfileImage);
 
-        final boolean hasPostImage = !TextUtils.isEmpty(tweet.getPostImageUrl());
-        mPostImage.setVisibility(hasPostImage ? View.VISIBLE : View.GONE);
-        if (hasPostImage) {
-	        Picasso.with(context)
-			        .load(tweet.getPostImageUrl())
-			        .placeholder(R.drawable.tweet_placeholder_image)
-			        .error(R.drawable.tweet_placeholder_image)
-			        .into(mPostImage);
-        }
-    }
+		final boolean hasPostImage = !TextUtils.isEmpty(tweet.getPostImageUrl());
+		mPostImage.setVisibility(hasPostImage ? View.VISIBLE : View.GONE);
+		if (hasPostImage) {
+			Picasso.with(context)
+					.load(tweet.getPostImageUrl())
+					.placeholder(R.drawable.tweet_placeholder_image)
+					.error(R.drawable.tweet_placeholder_image)
+					.into(mPostImage);
+		}
+	}
 
 	@Override
 	protected void dispatchDraw(Canvas canvas) {
 		super.dispatchDraw(canvas);
-		if(mChecked) {
-			canvas.drawColor(Color.argb(55, 255, 0, 0));
+
+		int ch = canvas.getHeight();
+		int cw = canvas.getWidth();
+		if (mChecked) {
+			canvas.drawRect(0, 0, cw, ch - 1, mSelectionPaint);
 		}
+
+		// divider line
+		canvas.drawRect(0, ch - 1, cw, ch, mDividerPaint);
 	}
 
 	@Override
